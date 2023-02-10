ce8a2aad511b05d5bff877ccc4303624217bbe29
Usama Mujib <usamau1998@gmail.com>
2023-02-10 12:51:51 -0800

    Introduction

---
+++ Loops.py
@@ -0,0 +1,42 @@
+# Task
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers i < n, print i**2.
+
+# Example
+
+# n = 3
+
+# The list of non-negative integers that are less than n = 3 is [0,1,2]. Print the square of each number on a separate line.
+
+# 0
+# 1
+# 4
+# Input Format
+
+# The first and only line contains the integer, n.
+
+# Constraints
+
+# 1 <= n <= 20
+
+# Output Format
+
+# Print n lines, one corresponding to each i.
+
+# Sample Input 0
+
+# 5
+# Sample Output 0
+
+# 0
+# 1
+# 4
+# 9
+# 16
+
+if __name__ == '__main__':
+    n = int(input())
+    y = 0
+    while (n > 0):
+        print(y**2)
+        y += 1
+        n -= 1
